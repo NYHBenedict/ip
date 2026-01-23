957e81d63778bbf0f574aede8c61ddbc3a7870ba
NYHBenedict <benedictngyuhao@gmail.com>
2026-01-23 12:39:17 +0800

    Level 1: echo commands and exit on bye

--- src/main/java/BenBot.java
+++ src/main/java/BenBot.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class BenBot {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("____________________________________________________________\n" +
                 " What's good! I'm BenBot \n" +
                 " What can I do for you today? \n" +
-                "____________________________________________________________\n" +
-                " Cya soon!\n" +
-                "____________________________________________________________");
+                "____________________________________________________________\n");
+
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine().trim();
+
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Cya soon! :)");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+            System.out.println();
+        }
+
+        sc.close();
     }
+
+
 }
