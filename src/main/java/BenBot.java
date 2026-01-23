fbac80779d369a0660b508af54f1acb0371cb9a9
NYHBenedict <benedictngyuhao@gmail.com>
2026-01-23 11:36:35 +0800

    Level 0 Done

---
+++ src/main/java/BenBot.java
@@ -0,0 +1,10 @@
+public class BenBot {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________\n" +
+                " What's good! I'm BenBot \n" +
+                " What can I do for you today? \n" +
+                "____________________________________________________________\n" +
+                " Cya soon!\n" +
+                "____________________________________________________________");
+    }
+}
