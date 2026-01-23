bd6b996cfedfd1325b2f32e5e931821b27d20bda
NYHBenedict <benedictngyuhao@gmail.com>
2026-01-23 12:49:33 +0800

    Level 2: store tasks and list them

--- src/main/java/BenBot.java
+++ src/main/java/BenBot.java
@@ -1,28 +1,63 @@
 import java.util.Scanner;
 
 public class BenBot {
+    private static final String LINE = "____________________________________________________________";
+
+    private static void printGreeting() {
+        printLine();
+        System.out.println("""
+                 What's good! I'm BenBot\s
+                 What can I do for you today?\s
+                """);
+        printLine();
+    }
+
+    private static void printLine() {
+        System.out.println(LINE);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("____________________________________________________________\n" +
-                " What's good! I'm BenBot \n" +
-                " What can I do for you today? \n" +
-                "____________________________________________________________\n");
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
+        printGreeting();
 
         while (sc.hasNextLine()) {
             String input = sc.nextLine().trim();
 
             if (input.equals("bye")) {
-                System.out.println("____________________________________________________________");
+                printLine();
                 System.out.println(" Cya soon! :)");
-                System.out.println("____________________________________________________________");
+                printLine();
                 break;
             }
 
-            System.out.println("____________________________________________________________");
-            System.out.println(" " + input);
-            System.out.println("____________________________________________________________");
-            System.out.println();
+            if (input.equals("list")) {
+                printLine();
+                if (taskCount == 0) {
+                    System.out.println("no tasks yet!");
+                } else {
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    }
+                }
+                printLine();
+                continue;
+            }
+
+            if (taskCount < 100) {
+                tasks[taskCount] = input;
+                taskCount++;
+                printLine();
+                System.out.println(" added: " + input);
+                printLine();
+            } else {
+                printLine();
+                System.out.println(" Too many tasks! ");
+                printLine();
+            }
         }
 
         sc.close();
